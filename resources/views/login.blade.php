466b1ee7c8fd6a143afd023294374c103c04d735
Lasauce6 <lasaucisse006@gmail.com>
2023-03-02 20:20:44 +0100

    login.blade.php email->login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,8 +22,8 @@
             <form action="{{ route('login.send') }}" method="post">
                 @csrf
                 <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email">
+                    <label for="email" class="form-label">Login</label>
+                    <input type="text" class="form-control" id="email" name="email">
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
