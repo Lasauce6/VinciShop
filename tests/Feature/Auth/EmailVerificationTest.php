d254542e2d9c3971a6cb2757482e375ed0b37a1c
Raphaël BATICLE <61377616+Lasauce6@users.noreply.github.com>
2023-10-03 17:50:58 +0200

    Vulnerabilities fix

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -36,7 +36,7 @@ class EmailVerificationTest extends TestCase
         $verificationUrl = URL::temporarySignedRoute(
             'verification.verify',
             now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1($user->email)]
+            ['id' => $user->id, 'hash' => bcrypt($user->email)]
         );
 
         $response = $this->actingAs($user)->get($verificationUrl);
@@ -55,7 +55,7 @@ class EmailVerificationTest extends TestCase
         $verificationUrl = URL::temporarySignedRoute(
             'verification.verify',
             now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1('wrong-email')]
+            ['id' => $user->id, 'hash' => bcrypt('wrong-email')]
         );
 
         $this->actingAs($user)->get($verificationUrl);
